b2b4b4f78e619c714896965cf0b87925b3869fd5
gralerfics <glverfer@outlook.com>
2024-10-04 01:37:24 +0800

    feat(ha02): update framework.

--- HA02_Linear_Regression/linear_regression.py
+++ HA02_Linear_Regression/linear_regression.py
@@ -2,43 +2,79 @@ import numpy as np
 
 
 """
-    [Loss] Mean Squared Error
+    [Optimizer] Gradient Descent Base Class
 """
-def MeanSquaredError(y_true, y_predict):
-    return np.mean((y_true - y_predict) ** 2)
+class GradientDescentOptimizer:
+    def __init__(self, learning_rate=0.001, max_iter=1000, tolerance=1e-6):
+        self.learning_rate = learning_rate
+        self.max_iter = max_iter
+        self.tolerance = tolerance
 
+        self.optim_vars: dict = None
+
+    def update(self, model, aux_vars):
+        pass
+
+    def optimize(self, model, aux_vars):
+        pass
 
 """
     [Optimizer] Stochastic Gradient Descent (SGD)
 """
-pass
+pass # TODO
 
 """
     [Optimizer] Batch Gradient Descent (BGD)
 """
-pass
+pass # TODO
 
 """
     [Optimizer] Mini-Batch Gradient Descent (MBGD)
 """
-pass
+pass # TODO
 
 
 """
     [Model] Linear Regression
 """
-pass
+class LinearRegression:
+    def __init__(self):
+        pass
+
+    # TODO
 
 
 """
-    Test the LinearRegression class
+    Data Preprocessing
+"""
+def random_split(X, y, test_size=0.2, random_state=None):
+    if random_state is not None:
+        np.random.seed(random_state)
+    
+    indices = np.arange(len(X))
+    np.random.shuffle(indices)
+
+    split_index = int(len(X) * (1 - test_size))
+    train_indices, test_indices = indices[:split_index], indices[split_index:]
+
+    X_train, y_train = X[train_indices], y[train_indices]
+    X_test, y_test = X[test_indices], y[test_indices]
+
+    return X_train, y_train, X_test, y_test
+
+
+"""
+    Test the code
 """
 if __name__ == "__main__":
     # Random data (from HA-02.pdf)
-    X_train = np.arange(10).reshape(100, 1)
+    X_train = np.arange(100).reshape(100, 1)
     a, b = 1, 10
     y_train = a * X_train + b + np.random.normal(0, 5, size=X_train.shape)
     y_train = y_train.reshape(-1)
+
+    # Split the data
+    X_train, y_train, X_test, y_test = random_split(X_train, y_train, test_size=0.2, random_state=42)
 
     # TODO
 
